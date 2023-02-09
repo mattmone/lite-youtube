fe77a6304ee663ba288558b3b7cc8714b8c64168
Matthew Mone <mmone@vernier.com>
2023-02-09 08:44:49 -0800

    anonymous changes to credentialless

--- lite-youtube.ts
+++ lite-youtube.ts
@@ -294,7 +294,7 @@ export class LiteYTEmbed extends HTMLElement {
       }
 
       const iframeHTML = `
-<iframe frameborder="0" ${this.hasAttribute('anonymous') ? 'anonymous' : ''}
+<iframe frameborder="0" ${this.hasAttribute('credentialless') ? 'credentialless' : ''}
   allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen
   src="https://www.youtube${wantsNoCookie}.com/embed/${embedTarget}autoplay=${autoplay}&${this.params}"
 ></iframe>`;
